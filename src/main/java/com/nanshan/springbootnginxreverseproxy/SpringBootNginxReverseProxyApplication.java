c1236c8e04e5845723aaa2901b59c7f34d72d588
Roger Lo <sobadrush@icloud.com>
2023-06-11 22:16:01 +0800

    [feature] 調整 spring-boot-starter-parent 版本至 2.7.8 ，避免 mvn compile 錯誤

---
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/SpringBootNginxReverseProxyApplication.java
@@ -0,0 +1,13 @@
+package com.nanshan.springbootnginxreverseproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNginxReverseProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNginxReverseProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanshan/springbootnginxreverseproxy/SpringBootNginxReverseProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanshan.springbootnginxreverseproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNginxReverseProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
