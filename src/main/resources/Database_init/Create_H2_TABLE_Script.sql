42530ac0ccf081dc0138b8d2da3cf1209f425ea6
Roger Lo <sobadrush@icloud.com>
2023-06-12 18:58:56 +0800

    新增建立DB的script

---
+++ src/main/resources/Database_init/Create_H2_TABLE_Script.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS DB_EMP_DEPT;
+SET SCHEMA DB_EMP_DEPT;
+
+----------------------------------------------------
+DROP TABLE IF EXISTS EMP_TB;
+DROP TABLE IF EXISTS DEPT_TB;
+----------------------------------------------------
+
+CREATE TABLE DEPT_TB
+(
+    DEPTNO INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    DNAME  VARCHAR(14),
+    LOC    VARCHAR(13)
+);
+
+CREATE TABLE EMP_TB
+(
+    EMPNO    INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    ENAME    VARCHAR(14),
+    JOB      VARCHAR(13),
+    HIREDATE DATE,
+    DEPTNO   INT NOT NULL,
+    FOREIGN KEY (DEPTNO) REFERENCES DEPT_TB (DEPTNO)
+    -- Hibernate做關聯查詢其實可以不用在實體表格設定foreign-key
+);
