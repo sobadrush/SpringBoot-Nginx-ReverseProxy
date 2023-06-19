c1d21f22ad0e5f84c3fd7130a910c10cd2c09ae3
Roger Lo <sobadrush@icloud.com>
2023-06-19 17:54:08 +0800

    [feature] docker-compose 1. 反向代理 nginx 設定 default.conf 2. SpringBoot 外部設定檔 application.yaml 3. SpringBoot 啟動類增加 log
    Signed-off-by: Roger Lo <sobadrush@icloud.com>

--- src/main/java/com/nanshan/springbootnginxreverseproxy/SpringBootNginxReverseProxyApplication.java
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/SpringBootNginxReverseProxyApplication.java
@@ -1,5 +1,6 @@
 package com.nanshan.springbootnginxreverseproxy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 
 /**
@@ -37,8 +39,17 @@ public class SpringBootNginxReverseProxyApplication implements CommandLineRunner
             .forEach(profile -> {
                 System.err.println("【Active Profile】：" + profile);
             });
+        System.err.println(MessageFormat.format("【SpringBoot】讀取 [外部設定檔]：{0}", this.getOuterSpringSettings()));
         System.err.println(" >>> email = " + email);
         System.err.println(" >>> applicationProps.getServers = " + applicationProps.getServers());
         System.err.println(applicationProps.getPurchase_info());
+    }
+
+    /**
+     * 取得 SpringBoot 外部設定路徑
+     */
+    private String getOuterSpringSettings() {
+        String springConfigLocation = System.getenv("SPRING_CONFIG_LOCATION");
+        return StringUtils.isNotBlank(springConfigLocation) ? springConfigLocation : "無(使用內部設定檔)";
     }
 }
