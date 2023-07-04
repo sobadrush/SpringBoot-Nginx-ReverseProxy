410067f4b853c21125217948270e9296ee3327ec
Roger Lo <sobadrush@icloud.com>
2023-07-04 17:01:42 +0800

    [feature] 測試 @Valid 參數校驗

--- src/main/java/com/nanshan/springbootnginxreverseproxy/controller/TestValidController.java
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/controller/TestValidController.java
@@ -1,10 +1,19 @@
 package com.nanshan.springbootnginxreverseproxy.controller;
 
+import com.nanshan.springbootnginxreverseproxy.model.DeptVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * @author RogerLo
@@ -16,4 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestValidController {
 
+    @PostMapping("/getDeptData1")
+    public ResponseEntity<DeptVO> getDeptData1(@Valid @RequestBody(required = false) DeptVO deptVO) {
+        log.info("deptVO: {}", deptVO);
+        return ResponseEntity.of(Optional.ofNullable(deptVO));
+    }
+
+    @PostMapping("/getDeptData2")
+    public ResponseEntity<?> getDeptData2(
+            @Valid @RequestBody(required = false) DeptVO deptVO, BindingResult bindingResult) {
+        log.info("deptVO: {}", deptVO);
+        if (bindingResult.hasErrors()) {
+            // 處理驗證錯誤
+            String errorMessage = bindingResult.getFieldErrors().stream()
+                    .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                    .reduce("", (s1, s2) -> s1 + "\n" + s2);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        }
+        return ResponseEntity.of(Optional.ofNullable(deptVO));
+    }
 }
--- src/main/java/com/nanshan/springbootnginxreverseproxy/model/DeptVO.java
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/model/DeptVO.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -25,12 +31,22 @@ public class DeptVO implements Serializable {
 
     @Id
     @Column(name = "DEPTNO", nullable = false, unique = true)
+    @NotNull(message = "部門編號不可為空")
+    @Min(value = 1000, message = "部門編號需 > 1000")
     private Long deptNo;
 
     @Column(name = "DNAME", nullable = true, unique = false)
+    @NotBlank(message = "部門名稱不可為空")
+    @Size(min = 2, max = 3)
     private String deptName;
 
     @Column(name = "LOC", nullable = true, unique = false)
+    @NotBlank(message = "部門地址不可為空")
     private String deptLoc;
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 
 }
