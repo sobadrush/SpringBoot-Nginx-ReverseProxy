32f39dbd6fc9c3f2841848fdec5012124573e78f
Roger Lo <sobadrush@icloud.com>
2023-07-17 10:02:01 +0800

    [BUG-FIX] 調整 ApplicationProps 設置

--- src/main/java/com/nanshan/springbootnginxreverseproxy/ApplicationProps.java
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/ApplicationProps.java
@@ -1,8 +1,8 @@
 package com.nanshan.springbootnginxreverseproxy;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
@@ -11,9 +11,10 @@ import java.util.Map;
  * @author RogerLo
  * @date 2023/6/18
  */
-@Component
+// @Component // 不需要
 @ConfigurationProperties(prefix = "application") // 對應 application.yaml 中的 key: application
-@Data // 將 yaml 中的值物件化，需搭配 get/setter
+@Getter
+@Setter // 將 yaml 中的值物件化，需搭配 get/setter
 public class ApplicationProps {
 
     // 對應到 application 下的 key: servers (屬性名稱要 mapping)
