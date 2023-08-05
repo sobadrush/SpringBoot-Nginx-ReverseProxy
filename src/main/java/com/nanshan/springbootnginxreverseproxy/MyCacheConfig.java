c3a929c945d34353caa0b5fb2d93c2020dd66207
Roger Lo <sobadrush@icloud.com>
2023-08-05 15:26:31 +0800

    修改註解

--- src/main/java/com/nanshan/springbootnginxreverseproxy/MyCacheConfig.java
+++ src/main/java/com/nanshan/springbootnginxreverseproxy/MyCacheConfig.java
@@ -28,7 +28,7 @@ public class MyCacheConfig {
         log.info("=== 設定 MyCacheConfig.caffeineConfig ===");
         return Caffeine.newBuilder()
                 // .expireAfterAccess(5, TimeUnit.SECONDS) // 表示項目在「最後一次訪問後」的【5秒】內保持有效。如果在【5秒】內再次訪問該項目，則計時器將被重置，並且項目的存留時間將從該訪問時間重新計算
-                .expireAfterWrite(3, TimeUnit.SECONDS) // 表示項目在「寫入快取後」的【5秒】 內保持有效。如果在【5秒】內再次訪問該項目，則計時器將被重置，並且項目的存留時間將從最後一次寫入時間重新計算
+                .expireAfterWrite(3, TimeUnit.SECONDS) // 表示項目在「寫入快取後」的【3秒】 內保持有效。如果在【5秒】內再次訪問該項目，則計時器將被重置，並且項目的存留時間將從最後一次寫入時間重新計算
                 // 參考[refreshAfterWrite requires a LoadingCache] 錯誤解決
                 // .refreshAfterWrite(3, TimeUnit.SECONDS) // 項目在寫入後的 3秒 內，當對該項目進行訪問時，Caffeine 快取會自動觸發刷新操作，以更新該項目的值
                 .initialCapacity(50) // 在創建快取時可以容納的項目數量。這個值用於預先分配內部數據結構的容量，以提高效能
